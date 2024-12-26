cecd1420dec75b958a647789df041faf36534795
Tony <wen114teng@gmail.com>
2024-12-27 00:40:10 +0800

    add right-arrow

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,27 @@ import React from 'react';
 import '../styles/Home.css';
 
 const Home = () => {
+
+  // Function to handle arrow click
+  const handleArrowClick = (direction) => {
+    // Implement the logic to navigate between screens
+    console.log(`Navigate ${direction}`);
+  };
+
   return (
     <div className="hero">
-      {/* 頭像移到左邊 */}
+
+      {/* 右箭頭 */}
+      <div className="arrow arrow-right" onClick={() => handleArrowClick('right')}>
+        <img src="/icons/right-arrow.png" alt="Right Arrow" />
+      </div>
+
+      {/* 左半(頭像) */}
       <div className="hero-avatar">
         <img src="/images/tony.jpeg" alt="Your Avatar" />
       </div>
-      {/* 介紹移到右邊 */}
+
+      {/* 右半部 */}
       <div className="hero-content">
         <p className="hero-intro">Hello! I am</p>
         <h1 className="hero-name">TONY KANG</h1>
@@ -20,12 +34,12 @@ const Home = () => {
           <a href="https://github.com/Ynot1996" target="_blank" rel="noopener noreferrer">
             <img src="/icons/github.png" alt="GitHub" />
           </a>
-          <a href="" target="_blank" rel="noopener noreferrer">
+          <a href="" target="mailto:wen114teng@gmail.com" rel="noopener noreferrer">
             <img src="/icons/gmail.png" alt="Gmail" />
           </a>
-          {/* 添加更多社交按鈕 */}
         </div>
       </div>
+
     </div>
   );
 };
