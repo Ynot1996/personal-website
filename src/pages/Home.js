e76dc1ddda059b9cfa1be3c907db46350af580d1
Tony <wen114teng@gmail.com>
2024-12-26 23:48:03 +0800

    Update Home.js and Home.css (icons added)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,19 +6,22 @@ const Home = () => {
     <div className="hero">
       {/* 頭像移到左邊 */}
       <div className="hero-avatar">
-        <img src="/images/your-image.jpg" alt="Your Avatar" />
+        <img src="/images/tony.jpeg" alt="Your Avatar" />
       </div>
       {/* 介紹移到右邊 */}
       <div className="hero-content">
         <p className="hero-intro">Hello! I am</p>
-        <h1 className="hero-name">Your Name</h1>
-        <p className="hero-tagline">Product Developer</p>
+        <h1 className="hero-name">TONY KANG</h1>
+        <p className="hero-tagline">Software Engineer</p>
         <div className="social-icons">
-          <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.linkedin.com/in/tony-kang-a353271bb/" target="_blank" rel="noopener noreferrer">
             <img src="/icons/linkedin.png" alt="LinkedIn" />
           </a>
-          <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+          <a href="https://github.com/Ynot1996" target="_blank" rel="noopener noreferrer">
             <img src="/icons/github.png" alt="GitHub" />
+          </a>
+          <a href="" target="_blank" rel="noopener noreferrer">
+            <img src="/icons/gmail.png" alt="Gmail" />
           </a>
           {/* 添加更多社交按鈕 */}
         </div>
