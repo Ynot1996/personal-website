01adb02ba1ad078dafdb0b14420189575f60948e
Tony <wen114teng@gmail.com>
2024-12-28 15:46:20 +0800

    update projects styles

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -6,14 +6,32 @@ const Projects = () => {
     {
       title: 'Focused group',
       description: '這是一個基於 React 和 Node.js 的個人網站，展示了我的技能與項目。',
-      image: 'https://via.placeholder.com/300x180', // 替換為實際圖片 URL
-      tags: ['React', 'Node.js', 'CSS'],
+      image: '/images/focused_group.png',
+      tags: ['HTML', 'CSS', 'javaScript'],
     },
     {
       title: 'Daliy Food Planning App',
       description: '使用 Python 和 Pandas 開發的數據分析工具，可視化複雜數據。',
       image: 'https://via.placeholder.com/300x180',
       tags: ['Python', 'Pandas', 'Data Visualization'],
+    },
+    {
+      title: 'Game',
+      description: '一個基於 React 的小型應用，展示了 Redux 的使用方法。',
+      image: 'https://via.placeholder.com/300x180',
+      tags: ['React', 'Redux', 'JavaScript'],
+    },
+    {
+      title: 'Game',
+      description: '一個基於 React 的小型應用，展示了 Redux 的使用方法。',
+      image: 'https://via.placeholder.com/300x180',
+      tags: ['React', 'Redux', 'JavaScript'],
+    },
+    {
+      title: 'Game',
+      description: '一個基於 React 的小型應用，展示了 Redux 的使用方法。',
+      image: 'https://via.placeholder.com/300x180',
+      tags: ['React', 'Redux', 'JavaScript'],
     },
     {
       title: 'Game',
