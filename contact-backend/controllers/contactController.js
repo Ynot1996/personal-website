7b3418fb18d4921f667dcf4fbaa66d87b3a60d6d
Tony <wen114teng@gmail.com>
2025-01-09 03:53:35 +0800

    update

--- contact-backend/controllers/contactController.js
+++ contact-backend/controllers/contactController.js
@@ -30,5 +30,4 @@ exports.handleContactForm = (req, res) => {
 
     // 模擬處理聯絡表單邏輯
     console.log(`Name: ${name}, Email: ${email}, Subject: ${subject}, Message: ${message}`);
-    res.status(200).json({ message: 'Form submission received!' });
 };
