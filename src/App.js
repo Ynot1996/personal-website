a1bc2d9bafbad20c571e30923406c8272719601f
Tony <wen114teng@gmail.com>
2025-01-14 23:42:39 +0800

    rwd humberger for homepage

--- src/App.js
+++ src/App.js
@@ -7,28 +7,37 @@ import Contact from './pages/Contact';
 import './styles/App.css';
 
 function App() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
     <Router>
 
       <nav className="navbar">
-
+        {/* Logo */}
         <Link to="/personal-website" className="nav-logo">
           <img src={`${process.env.PUBLIC_URL}/images/logo.png`} alt="Logo" />
         </Link>
 
+        {/* Hamburger Menu */}
+        <div className="hamburger-menu" onClick={toggleMobileMenu}>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+
+        {/* Navigation Links */}
         <ul className="nav-links">
           <li><Link to="/personal-website" className="nav-link">Home</Link></li>
-
           <li><Link to="/personal-website/about" className="nav-link">About</Link></li>
-
           <li><Link to="/personal-website/projects" className="nav-link">Projects</Link></li>
-
           {/* 暫時隱藏 Skills */}
           {/* <li><Link to="/personal-website/skills" className="nav-link">Skills</Link></li> */}
-
           <li><Link to="/personal-website/contact" className="nav-link">Contact</Link></li>
         </ul>
-
       </nav>
 
       <Routes>
